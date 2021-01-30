3a0a2061f9bda1c719b98af44351b467c075efcf
geoffroygoirand <geoffroy.goirand@gmail.com>
2021-01-30 15:31:37 +0100

    on a fait les routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :dragons only %i[:index :create :new :update :edit :show] 
+  resources :bookings only %i[:index :create :new :update :edit :show] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
