5c8ebaadf922510417c66d1c85be638a7085d166
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-01-30 21:49:51 +0100

    Ajout fichier avec 10 users 10 dragons 10 bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Booking.destroy_all
+# Dragon.destroy_all
+# User.destroy_all
+
+
+
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.unique.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(number: 10, min_alpha: 4),
+    role: ["owner", "renter"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true)
+    )
+end
+
+10.times do
+   dragon = Dragon.create(
+    nickname: Faker::JapaneseMedia::DragonBall.unique.character,
+    country: ["France", "Iceland", "Alaska", "Argentina", "Botswana", "New Zealand", "China", "Thailand", "Japan", "Niger", "Iran"].sample,
+    size: %w(small medium large xlarge).sample,
+    speed: %w(slow medium fast superfast).sample,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true),
+    user_id: (1..10).to_a.sample
+    )
+end
+
+10.times do
+   booking = Booking.create(
+    user_id: (1..10).to_a.sample,
+    dragon_id: (1..10).to_a.sample,
+    status: ["pending", "cancelled", "accepted", "done"].sample,
+    start_date: Faker::Date.in_date_period(year: 2021, month: 3),
+    end_date: Faker::Date.in_date_period(year: 2021, month: 4)
+    )
+end
+
+
