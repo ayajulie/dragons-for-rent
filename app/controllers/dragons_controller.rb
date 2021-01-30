38399f93a1dff40e4251d4eb2cde05c8efc3b906
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-01-30 23:10:47 +0100

    Correction routes, création page montrer 1 dragon

--- app/controllers/dragons_controller.rb
+++ app/controllers/dragons_controller.rb
@@ -1,2 +1,15 @@
 class DragonsController < ApplicationController
+skip_before_action :authenticate_user!, only: :show
+
+  def show
+    @dragon = Dragon.find(params[:id])
+  end
+
+  def new
+    @dragon = Dragon.new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :dragons, only: %i[:index :create :new :update :edit :show] 
-  resources :bookings,  only: %i[:index :create :new :update :edit :show] 
+  resources :dragons, except: :destroy
+  resources :bookings, except: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
