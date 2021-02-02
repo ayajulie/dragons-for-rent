a286ed86215ae983b5f769a48b8bc18e0be18f06
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-02-02 20:57:32 +0100

    Ajout des routes update et edit dans routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :dragons, except: :destroy do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :edit, :update]
   end
 
   resources :bookings, only: [:destroy]
